e769113238665647f0e1e417c4c2efd938d6aed4
Priyanka <84381308+priyanka77-priya@users.noreply.github.com>
2022-11-23 22:54:06 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Fun1 from './component/fun1';
+import Count1 from './component/count';
+
+51
+
+function App() {
+return (
+<div>
+<Fun1/>
+<Count1/>
+</div>
+);
+}
+export default App;
