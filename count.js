31b0e63fc1f4ff8db7dba44a507e47ad57af7427
Priyanka <84381308+priyanka77-priya@users.noreply.github.com>
2022-11-23 22:58:25 +0530

    Update count.js

--- count.js
+++ count.js
@@ -1,5 +1,29 @@
-import React from "react";
-function Fun1() {
-return <h1> Simple Counter </h1>
+import React, { Component } from 'react';
+
+class Count1 extends Component {
+constructor(props) {
+super(props)
+
+this.state = {
+count:0
+};
 }
-export default Fun1;
+increment=()=>{this.setState({count:this.state.count+1});
+};
+decrement=()=>{this.setState({count:this.state.count-1});
+};
+render() {
+return (
+<div className='App'>
+
+52
+
+<button onClick={this.increment} className='counter'>+</button>
+&nbsp;
+<button onClick={this.decrement} className='counter'>-</button>
+<h2>Count : {this.state.count}</h2>
+</div>
+);
+}
+}
+export default Count1;
